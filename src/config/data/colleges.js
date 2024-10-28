2b33d97da7e935c902bc418bcde3225145e41ea8
Srishty Mangutte <srishtymangutte@gmail.com>
2024-10-29 02:12:02 +0530

    fix: update colleges list with reference to frontend

--- src/config/data/colleges.js
+++ src/config/data/colleges.js
@@ -1,119 +1,68 @@
 const colleges = {
-  "671e4252cf42c86749354c1c": "VIT-AP University",
-  "671e2097cf42c86749354bf9": "Badriprasad Institute of Technology, Sambalpur",
-  "671e205ecf42c86749354bf7": "Bhubaneswar Engineering College (BEC BBSR)",
-  "671e17f2cf42c86749354bd9":
-    "Odisha University of Technology and Research (OUTR)",
-  "671e0e7dcf42c86749354bc6":
+  "671a5be16748c70b7f893caa":
     "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671e0cedcf42c86749354bc2": "Badriprasad Institute of Technology, Sambalpur",
-  "671e0b7dcf42c86749354bbc":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671e0885cf42c86749354bba": "Badriprasad Institute of Technology, Sambalpur",
-  "671dfe5ecf42c86749354baf":
+  "671a5be16748c70b7f893cab":
     "Odisha University of Technology and Research (OUTR)",
-  "671df5b1cf42c86749354bad": "Biju Patnaik University of Technology (BPUT)",
-  "671df506cf42c86749354bab":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dee7acf42c86749354ba9":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671de976cf42c86749354ba5":
-    "National Institute of Technology, Rourkela (NIT Rourkela)",
-  "671de294cf42c86749354ba3":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671ddf0bcf42c86749354ba1":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dddf3cf42c86749354b9f":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dddc1cf42c86749354b9d":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dddc0cf42c86749354b9b":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671ddb14cf42c86749354b99": "Indira Gandhi National Open University",
-  "671dc677cf42c86749354b97":
-    "Trident Academy of Technology, Bhubaneswar (Trident BBSR)",
-  "671dc34ccf42c86749354b95":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dc2b5cf42c86749354b93":
-    "Trident Academy of Technology, Bhubaneswar (Trident BBSR)",
-  "671dc1decf42c86749354b91":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dc163cf42c86749354b8f":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671dbcedcf42c86749354b8d": "Siksha O Anusandhan",
-  "671dbac3cf42c86749354b8b":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671db960cf42c86749354b89":
-    "International Institute of Information Technology, Bhubaneswar (IIIT BBSR)",
-  "671db3f1cf42c86749354b87":
-    "Odisha University of Technology and Research (OUTR)",
-  "671db1e5cf42c86749354b85":
-    "Odisha University of Technology and Research (OUTR)",
-  "671dabb6cf42c86749354b83":
-    "Odisha University of Technology and Research (OUTR)",
-  "671dab27cf42c86749354b81":
-    "Odisha University of Technology and Research (OUTR)",
-  "671dab02cf42c86749354b7f":
-    "Odisha University of Technology and Research (OUTR)",
-  "671dab01cf42c86749354b7d":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daaffcf42c86749354b7b":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daaf8cf42c86749354b79":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daadfcf42c86749354b77":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daad7cf42c86749354b75":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daad4cf42c86749354b73":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daa6ccf42c86749354b71":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daa6bcf42c86749354b6f":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daa6acf42c86749354b6d":
-    "Odisha University of Technology and Research (OUTR)",
-  "671daa67cf42c86749354b6b":
-    "Odisha University of Technology and Research (OUTR)",
-  "671da975cf42c86749354b69":
-    "Odisha University of Technology and Research (OUTR)",
-  "671da972cf42c86749354b67":
-    "Odisha University of Technology and Research (OUTR)",
-  "671da932cf42c86749354b65":
-    "Odisha University of Technology and Research (OUTR)",
-  "671da92bcf42c86749354b63":
-    "Odisha University of Technology and Research (OUTR)",
-  "671da91acf42c86749354b61":
-    "Odisha University of Technology and Research (OUTR)",
-  "671da137cf42c86749354b5f":
-    "National Institute of Technology, Rourkela (NIT Rourkela)",
-  "671d53c8cf42c86749354b5d":
-    "National Institute of Technology, Rourkela (NIT Rourkela)",
-  "671d4332cf42c86749354b5a": "Badriprasad Institute of Technology, Sambalpur",
-  "671d40c3cf42c86749354b58":
-    "National Institute of Technology, Rourkela (NIT Rourkela)",
-  "671d401ecf42c86749354b56":
-    "National Institute of Technology, Rourkela (NIT Rourkela)",
-  "671d3fedcf42c86749354b54": "bhhbhbh",
-  "671d37b2cf42c86749354b50":
+  "671a5be16748c70b7f893cac":
+    "Gandhi Institute of Engineering and Technology, Gunupur (GIET Gunupur)",
+  "671a5be26748c70b7f893cad":
+    "Centurion University of Technology and Management",
+  "671a5be26748c70b7f893cae": "Biju Patnaik University of Technology (BPUT)",
+  "671a5be26748c70b7f893caf":
+    "National Institute of Science and Technology, Berhampur (NIST Berhampur)",
+  "671a5be26748c70b7f893cb0":
+    "Kalinga Institute of Industrial Technology, Bhubaneswar (KIIT BBSR)",
+  "671a5be26748c70b7f893cb1":
     "C. V. Raman Global University, Bhubaneswar (CV RAMAN)",
-  "671d34fdcf42c86749354b4e": "Birla Institute of Technology, Durg (BIT Durg)",
-  "671d2e5fcf42c86749354b3e": "bitr",
-  "671d2bffcf42c86749354b3c": "Biju Patnaik University of Technology (BPUT)",
-  "671d26f265acc58ee07a21f7": "Biju Patnaik University of Technology (BPUT)",
-  "671d17d265acc58ee07a21ec":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671d17c465acc58ee07a21eb":
-    "Silicon Institute of Technology, Bhubaneswar (SIT-BBSR)",
-  "671bb67e578281c65287a545": "Innovision '24",
-  "671a5be86748c70b7f893cce":
-    "Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur)",
-  "671a5be86748c70b7f893ccd":
-    "Trident Academy of Technology, Bhubaneswar (Trident BBSR)",
+  "671a5be36748c70b7f893cb2": "Parala Maharaja Engineering College",
+  "671a5be36748c70b7f893cb3": "Indira Gandhi Institute of Technology, Sarang",
+  "671a5be36748c70b7f893cb4":
+    "Veer Surendra Sai University of Technology (VSSUT)",
+  "671a5be36748c70b7f893cb5":
+    "Gandhi Institute for Technological Advancement (GITA)",
+  "671a5be36748c70b7f893cb6": "Pragati Maidan Engineering College (PMEC)",
+  "671a5be46748c70b7f893cb7": "Bhubaneswar Engineering College (BEC BBSR)",
+  "671a5be46748c70b7f893cb8":
+    "Indian Institute of Science Education and Research, Berhampur (IISER Berhampur)",
+  "671a5be46748c70b7f893cb9":
+    "International Institute of Information Technology, Bhubaneswar (IIIT BBSR)",
+  "671a5be46748c70b7f893cba":
+    "Indian Institute of Technology, Bhubaneswar (IIT BBSR)",
+  "671a5be46748c70b7f893cbb":
+    "Rajendra Institute of Medical Sciences, Rourkela (RIMS, Rourkela)",
+  "671a5be56748c70b7f893cbc": "Municipal College, Rourkela",
+  "671a5be56748c70b7f893cbd": "Government Autonomous College, Rourkela",
+  "671a5be56748c70b7f893cbe": "Badriprasad Institute of Technology, Sambalpur",
+  "671a5be56748c70b7f893cbf": "Sambalpur University",
+  "671a5be56748c70b7f893cc0": "Utkalmani Gopabandhu College",
+  "671a5be66748c70b7f893cc1":
+    "Government College of Engineering, Kalahandi, Bhawanipatna (GCEK Bhawanipatna)",
+  "671a5be66748c70b7f893cc2": "Birla Institute of Technology, Durg (BIT Durg)",
+  "671a5be66748c70b7f893cc3":
+    "Birla Institute of Technology, Mesra (BIT Mesra)",
+  "671a5be66748c70b7f893cc4":
+    "National Institute of Technology, Jamshedpur (NIT Jamshedpur)",
+  "671a5be66748c70b7f893cc5":
+    "National Institute of Technology, Durgapur (NIT Durgapur)",
+  "671a5be66748c70b7f893cc6":
+    "Indian Institute of Technology, Dhanbad (IIT Dhanbad)",
+  "671a5be76748c70b7f893cc7":
+    "Indian Institute of Technology, Bhilai (IIT Bhilai)",
+  "671a5be76748c70b7f893cc8":
+    "National Institute of Technology, Raipur (NIT Raipur)",
+  "671a5be76748c70b7f893cc9": "Jadavpur University",
+  "671a5be76748c70b7f893cca":
+    "National Institute of Technology, Patna (NIT Patna)",
+  "671a5be76748c70b7f893ccb":
+    "National Institute of Technology, Rourkela (NIT Rourkela)",
+  "671a5be76748c70b7f893ccc":
+    "National Institute of Technology, Andhra Pradesh (NIT Andhra Pradesh)",
   "671a5be86748c70b7f893ccc":
     "Vellore Institute of Technology, Vellore (VIT Vellore)",
-  "671a5be76748c70b7f893ccb":
-    "National Institute of Technology, Andhra Pradesh (NIT Andhra Pradesh)",
+  "671a5be86748c70b7f893ccd":
+    "Trident Academy of Technology, Bhubaneswar (Trident BBSR)",
+  "671a5be86748c70b7f893cce":
+    "Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur)",
 };
 
 export default colleges;
